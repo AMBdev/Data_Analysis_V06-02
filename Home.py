c874e6c65eed0fe5873655fd6f5cb52b188c60bb
AMBdev <40493020+AMBdev@users.noreply.github.com>
2025-05-06 09:04:11 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ Created on Mon Apr 24 18:50:15 2023
 @author: a863900
 """
 import streamlit as st
-import streamlit_aggrid as st_aggrid
+import streamlit-aggrid as st_aggrid
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 import numpy as np
 import pytz
